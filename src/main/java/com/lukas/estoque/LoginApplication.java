ed25623b75c62f998ee1db26e271519bed36c282
lukasvieira2 <lukascarneirotavares20@gmail.com>
2026-07-28 17:40:47 -0300

    primeiro commit

--- src/main/java/com/lukas/estoque/LoginApplication.java
+++ src/main/java/com/lukas/estoque/LoginApplication.java
@@ -3,6 +3,7 @@ package com.lukas.estoque;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class LoginApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Login");
+        stage.getIcons().add( new Image(LoginApplication.class.getResourceAsStream("img/in-stock.png")));
         stage.setScene(scene);
         stage.show();
     }
